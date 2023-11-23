d4e11f67d98626168820fd4ece1fdf0616d97a7b
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-11-23 14:00:16 +0100

    Updated some dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import React from "react";
 
 const inter = Inter({
   subsets: ["latin"],
