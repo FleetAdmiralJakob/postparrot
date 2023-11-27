28037b421851e519a12eac4145ce0c8e140135af
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-11-27 20:15:17 +0100

    Cleaned up the comments on the schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,3 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
 import { sql } from "drizzle-orm";
 import {
   index,
@@ -11,8 +8,7 @@ import {
 } from "drizzle-orm/pg-core";
 
 /**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
+ * With this, you can use the same database instance for multiple projects.
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
