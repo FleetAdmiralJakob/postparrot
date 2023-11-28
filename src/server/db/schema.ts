34c7b0766314a4d04b3faaf8abdf25ce31a503bf
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-11-28 21:23:24 +0100

    Updated the schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
   index,
   pgTableCreator,
@@ -18,14 +18,39 @@ export const posts = pgTable(
   "post",
   {
     id: uuid("id").defaultRandom().primaryKey(),
-    content: varchar("content", { length: 256 }).notNull(),
+    content: varchar("content", { length: 300 }).notNull(),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: timestamp("updatedAt").default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: timestamp("updated_at").default(sql`CURRENT_TIMESTAMP`),
     userId: varchar("user_id").notNull(),
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.content),
+    userIdIndex: index("user_id_idx").on(example.userId),
   }),
 );
+
+export const postsRelations = relations(posts, ({ many }) => ({
+  hearts: many(hearts),
+}));
+
+export const hearts = pgTable(
+  "heart",
+  {
+    id: uuid("id").defaultRandom().primaryKey(),
+    postId: uuid("post_id").notNull(),
+    userId: varchar("user_id").notNull(),
+  },
+  (example) => ({
+    postIdIndex: index("post_id_idx").on(example.postId),
+    userIdIndex: index("user_id_idx").on(example.userId),
+  }),
+);
+
+export const heartsRelations = relations(hearts, ({ one }) => ({
+  post: one(posts, {
+    fields: [hearts.postId],
+    references: [posts.id],
+  }),
+}));
