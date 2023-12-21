6a96308205457bd912af2a1389e3a316bfd1d22c
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-12-21 17:52:40 +0100

    feat: Added an offline fallback page and updated some dependencies

---
+++ src/app/~offline/page.tsx
@@ -0,0 +1,13 @@
+const OfflinePage = () => {
+  return (
+    <div className="flex w-full flex-col items-center">
+      <h1 className="mt-10 text-4xl font-bold">Offline</h1>
+      <p>
+        It looks like you are offline. Please check your internet connection and
+        try again.
+      </p>
+    </div>
+  );
+};
+
+export default OfflinePage;
