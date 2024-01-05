a0aaf842cdca78ced0c564127bd51c3312b9eded
FleetAdmiralJakob <jakob.roessner@outlook.de>
2024-01-05 23:28:07 +0100

    fix(deps + middleware): Added the ~offline page to the ones you can access without signing in and updated some dependencies.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/_vercel/speed-insights/vitals"],
+  publicRoutes: ["/", "/_vercel/speed-insights/vitals", "/~offline"],
 });
 
 export const config = {
